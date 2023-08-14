77ac5fbd0a8ff6c47db41f57bc7f96a249f12470
Ezeko95 <ciangianluca@hotmail.com>
2023-08-14 17:07:54 -0300

    googleAuth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 axios.defaults.baseURL = "http://127.0.0.1:5000";
 
+const clientId =
+  "825644132093-0f3l25v9u5858itva6f5094anqlk2id6.apps.googleusercontent.com";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <GoogleOAuthProvider clientId={clientId}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
